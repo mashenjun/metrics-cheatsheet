3b1e3d3a5b2bc3eb7ba846aa534c803be3af3220
mashenjun <mashenjun0902@gmail.com>
2021-03-10 18:23:50 +0000

    Deploying to gh-pages from  @ 086200ec1dbfd8204c1d4aa42c3a6523113925c2 🚀

--- static/table.js
+++ static/table.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", function () {
-    for (var _i = 0, _a = $$(".table-row"); _i < _a.length; _i++) {
-        var tr = _a[_i];
+    var _loop_1 = function (tr) {
         var tableRow = tr;
         tableRow.onclick = function () {
             var aElement = tableRow.getElementsByTagName("a")[0];
             window.location.href = aElement.getAttribute("href");
         };
+    };
+    for (var _i = 0, _a = $$(".table-row"); _i < _a.length; _i++) {
+        var tr = _a[_i];
+        _loop_1(tr);
     }
 });
